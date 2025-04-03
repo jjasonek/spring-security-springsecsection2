27ac31e1e26ed71c6836930ceb0f743008a3f06b
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-04-03 23:45:15 +0200

    Customize secured and not secured requests in ProjectSecurityConfig

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -14,14 +14,15 @@ public class ProjectSecurityConfig {
      * We copied this method from
      * org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration
      * and customised it.
-     *
-     * @param http
-     * @return
-     * @throws Exception
+     * The error page is the default Spring page when an error occurs. We should "secure" it too.
      */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans, /myCards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
