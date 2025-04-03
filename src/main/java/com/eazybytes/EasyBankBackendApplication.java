6cef1afc271968e4a5b0b42e699409f1a4a1a989
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-04-03 17:02:08 +0200

    Clean comments

--- src/main/java/com/eazybytes/EasyBankBackendApplication.java
+++ src/main/java/com/eazybytes/EasyBankBackendApplication.java
@@ -2,11 +2,8 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//this would bee needed when the controller package was outside the com.eazybytes.springsecsection1 package.
-//@ComponentScan("com.eazybytes.springsecsection1.controller")
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
