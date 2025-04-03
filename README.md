93f1e913bc3340357d83935e1768678677592c8e
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-04-03 22:58:11 +0200

    Add ProjectSecurityConfig for customized security

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Udemy Course Spring Security Section2
 ## spring version: 3.4.4
 
+## First phase of DefaultSecurityFilterChain customization 
+Just copied the method form SpringBootWebSecurityConfiguration class to our custom class.
---
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    /**
+     * We copied this method from
+     * org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration
+     * and customised it.
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
